4589e1afa21a495f462183d2e113aedfa90f3842
nperello <nperello@live.com>
2016-10-26 18:34:08 -0400

    Add files via upload

---
+++ code2040.py
@@ -0,0 +1,7 @@
+from urllib2 import Request, urlopen, URLError
+import json, requests
+
+t= '18f7ae041f26ac48d019bcfbb5f4ab4f'
+mygit= 'https://github.com/nperello/2040'
+request = requests.post('http://challenge.code2040.org/api/register', json={'token':t,'github':mygit})
+print(request.text)
