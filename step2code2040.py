70e9d08c16259e5a9c315310f139a55c7166270c
nperello <nperello@live.com>
2016-10-26 18:57:57 -0400

    step 2 first

--- step2code2040.py
+++ step2code2040.py
@@ -2,3 +2,8 @@ import json, requests
 
 def reverseString(word):
     return word[::-1] #just slice it
+
+myToken = '18f7ae041f26ac48d019bcfbb5f4ab4f'
+request = requests.post('http://challenge.code2040.org/api/reverse',json={'token':myToken})
+reverse = reverseString(request.text)
+request1 = requests.post('http://challenge.code2040.org/api/reverse/validate',json={'token':myToken,'string':reverse})
