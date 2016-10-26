25f7c8bb5d468d94c789fae403a4dd5f1a21c124
nperello <nperello@live.com>
2016-10-26 19:35:20 -0400

    step 3

---
+++ step3code2040.py
@@ -0,0 +1,16 @@
+import json, requests
+
+
+myToken = '18f7ae041f26ac48d019bcfbb5f4ab4f'
+request = requests.post('http://challenge.code2040.org/api/haystack',json={'token':myToken})
+arry = json.loads(request.text)
+needle = arry['needle']
+hay = arry['haystack']
+index = 0
+for x in hay:
+    if x == needle:
+        break
+    else:
+        index+=1
+request1 = requests.post('http://challenge.code2040.org/api/haystack/validate',json={'token':myToken,'needle':index})
+print(request1.text)
