79a748b57faa6d708f416d2ce025e07ca7bf1874
nperello <nperello@live.com>
2016-10-26 18:55:00 -0400

    cleaning up step 1

--- step1code2040.py
+++ step1code2040.py
@@ -1,6 +1,6 @@
 import json, requests
 
-t= '18f7ae041f26ac48d019bcfbb5f4ab4f'
-mygit= 'https://github.com/nperello/2040'
-request = requests.post('http://challenge.code2040.org/api/register', json={'token':t,'github':mygit})
+myToken = '18f7ae041f26ac48d019bcfbb5f4ab4f'
+myGit= 'https://github.com/nperello/2040'
+request = requests.post('http://challenge.code2040.org/api/register', json={'token':myToken,'github':myGit})
 print(request.text)
