1f710118c8526eacc61381a7a3b3b9f60667550d
nperello <nperello@live.com>
2016-10-26 18:58:35 -0400

    step 2 done

--- step2code2040.py
+++ step2code2040.py
@@ -7,3 +7,4 @@ myToken = '18f7ae041f26ac48d019bcfbb5f4ab4f'
 request = requests.post('http://challenge.code2040.org/api/reverse',json={'token':myToken})
 reverse = reverseString(request.text)
 request1 = requests.post('http://challenge.code2040.org/api/reverse/validate',json={'token':myToken,'string':reverse})
+print(request1.text)
