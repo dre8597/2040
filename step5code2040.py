cd9866e8b385c623006042d168e8ce97dcf52a56
nperello <nperello@live.com>
2016-10-26 20:50:57 -0400

    fixes

--- step5code2040.py
+++ step5code2040.py
@@ -1,6 +1,10 @@
-import json, requests
+import json, requests, datetime, dateutil.parser
 
 
 myToken = '18f7ae041f26ac48d019bcfbb5f4ab4f'
 request = requests.post('http://challenge.code2040.org/api/dating',json={'token':myToken})
 arry = json.loads(request.text)
+date= arry['datestamp']
+timeAdd = arry['interval']
+print dateutil.parser.parse(date)
+print date
